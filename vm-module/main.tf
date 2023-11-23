ed19c7478bc1a74361d19c3af9b3fb69640ecbb7
Andrey Sidorov <13andry06@gmail.com>
2023-11-23 12:52:56 +0400

    Initial commit of VM module code and inputs

---
+++ vm-module/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "terrform-vm" {
+  name        = var.vm_name
+  desc        = var.description
+  target_node = each.value.target_node
+  os_type     = "cloud-init"
+  full_clone  = true
+  memory      = var.memory
+  sockets     = var.sockets
+  cores       = var.cores
+  cpu         = "host"
+  scsihw      = "virtio-scsi-pci"
+  clone       = var.k8s_source_template
+  agent       = 1
+  disk {
+    size    = var.disk_size
+    type    = "scsi"
+    storage = var.storage_pool
+  }
+  network {
+    model  = "virtio"
+    bridge = var.bridge
+    tag    = var.vlan
+  }
+
+  # Cloud-init section
+  ipconfig0 = "ip=dhcp"
+  sshkeys   = var.ssh_keys
+}
+
--- vm-module/variables.tf
+++ vm-module/variables.tf
@@ -8,9 +8,67 @@ variable "vm_name" {
   type        = string
 }
 
-
+variable "target_node" {
+  description = "The node to use when creating VM"
+  type        = string
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
+
+variable "description" {
+  description = "The description for the VM that will be created"
+  type        = string
+  default     = "Managed by Terraform"
+}
+
+variable "memory" {
+  description = "The amount of memory to allocate to the VM in Megabytes"
+  type        = string
+  default     = "1024"
+}
+
+variable "cores" {
+  description = "The number of CPU cores per CPU socket to allocate to the VM"
+  type        = string
+  default     = "1"
+}
+
+variable "sockets" {
+  description = "The number of CPU sockets to allocate to the VM"
+  type        = string
+  default     = "1"
+
+}
+
+variable "disk_size" {
+  description = "The size of the created disk"
+  type        = string
+  default     = "16G"
+}
+
+variable "storage_pool" {
+  description = "The name of the storage pool on which to store the disk."
+  type        = string
+  default     = "local-lvm"
+}
+
+variable "vlan" {
+  description = "VLAN to use for network adapter"
+  type        = string
+  default     = "1"
+}
+
+variable "bridge" {
+  description = "Bridge to which the network device should be attached"
+  type        = string
+  default     = "vmbr0"
+}
+
+variable "ssh_keys" {
+  description = "Newline delimited list of SSH public keys to add to authorized keys file for the cloud-init user."
+  type        = string
+  default     = null
+}
