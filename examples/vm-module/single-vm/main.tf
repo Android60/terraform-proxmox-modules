0c71c8688c667abe0b991199b8d685c946a9c07a
Andrey Sidorov <13andry06@gmail.com>
2023-11-23 13:19:26 +0400

    Added basic example of using VM module

--- examples/vm-module/single-vm/main.tf
+++ examples/vm-module/single-vm/main.tf
@@ -1,8 +1,21 @@
 terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
 }
 
+provider "proxmox" {
+  pm_tls_insecure = true
+  pm_timeout      = 900
+}
+
+
 module "proxmox_vm" {
-  source  = "../../../vm-module/"
-  vm_name = var.vm_name
+  source         = "../../../vm-module/"
+  vm_name        = var.vm_name
+  target_node    = var.target_node
+  clone_template = var.clone_template
 }
