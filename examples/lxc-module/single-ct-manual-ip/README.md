5b2b586dd3c816519c75bef41b8eddb71abe7195
Andrey Sidorov <13andry06@gmail.com>
2023-11-24 18:11:04 +0400

    Added single-ct-manual-ip example

---
+++ examples/lxc-module/single-ct-manual-ip/README.md
@@ -0,0 +1,4 @@
+# Single LXC example
+
+This folder contains a [Terraform](https://www.terraform.io/) configuration that shows an example of how to 
+use the [LXC module](../../../modules/vm-module/) to deploy a LXC container with manual IP config and VLAN in Proxmox.
---
+++ examples/lxc-module/single-ct-manual-ip/variables.tf
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "lxc_name" {
+  description = "The name of the VM that will be created"
+  type        = string
+  default     = "example-lxc-with-manual-ip-vlan-module"
+}
+
+variable "target_node" {
+  description = "The node to use when creating VM"
+  type        = string
+  default     = "ibm-x3200-prox2"
+}
+
+variable "source_template" {
+  description = "The base VM from which to clone to create the new VM"
+  type        = string
+  default     = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+}
+
+variable "root_password" {
+  description = "Root password inside the container"
+  type        = string
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+
+variable "description" {
+  description = "The description for the VM that will be created"
+  type        = string
+  default     = "Managed by Terraform"
+}
+
