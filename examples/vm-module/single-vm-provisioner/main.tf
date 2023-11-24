630d0ba37a484466066e8a3afd957b2b496fbe0f
Andrey Sidorov <13andry06@gmail.com>
2023-11-24 17:40:49 +0400

    Rename vm_name variable

--- examples/vm-module/single-vm-provisioner/main.tf
+++ examples/vm-module/single-vm-provisioner/main.tf
@@ -15,7 +15,7 @@ provider "proxmox" {
 
 module "proxmox_vm" {
   source         = "../../../modules/vm-module/"
-  vm_name        = var.vm_name           # Name for new VM
+  name           = var.vm_name           # Name for new VM
   target_node    = var.target_node       # Create VM on that node
   clone_template = var.clone_template    # Template to use when cloning VM
   remote_exec    = var.remote_exec       # Script to execute on provisioning
