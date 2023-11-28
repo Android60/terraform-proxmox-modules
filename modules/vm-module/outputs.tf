319914f831c2307b4b96e9880d7bebc194ae2669
Andrey Sidorov <13andry06@gmail.com>
2023-11-28 20:50:49 +0400

    Output VM ID in VM module

--- modules/vm-module/outputs.tf
+++ modules/vm-module/outputs.tf
@@ -1,3 +1,7 @@
 output "ip_address" {
   value = try(proxmox_vm_qemu.terraform-vm[0].default_ipv4_address, proxmox_vm_qemu.terraform-vm-provisioner[0].default_ipv4_address)
 }
+
+output "vmid" {
+  value = try(proxmox_vm_qemu.terraform-vm[0].vmid, proxmox_vm_qemu.terraform-vm-provisioner[0].vmid)
+}
