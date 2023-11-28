1d8d890fa98af8f0dc81ce251361992dd1a5133c
Andrey Sidorov <13andry06@gmail.com>
2023-11-28 20:57:07 +0400

    Added description for outputs

--- modules/vm-module/outputs.tf
+++ modules/vm-module/outputs.tf
@@ -1,7 +1,9 @@
 output "ip_address" {
-  value = try(proxmox_vm_qemu.terraform-vm[0].default_ipv4_address, proxmox_vm_qemu.terraform-vm-provisioner[0].default_ipv4_address)
+  value       = try(proxmox_vm_qemu.terraform-vm[0].default_ipv4_address, proxmox_vm_qemu.terraform-vm-provisioner[0].default_ipv4_address)
+  description = "IP address of VM"
 }
 
 output "vmid" {
-  value = try(proxmox_vm_qemu.terraform-vm[0].vmid, proxmox_vm_qemu.terraform-vm-provisioner[0].vmid)
+  value       = try(proxmox_vm_qemu.terraform-vm[0].vmid, proxmox_vm_qemu.terraform-vm-provisioner[0].vmid)
+  description = "VM ID"
 }
